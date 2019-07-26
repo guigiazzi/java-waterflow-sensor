2888aaea4c785abdf94c63dc67f5c526dc5a7629
Guilherme Giazzi <guigiazzi@hotmail.com>
2019-07-26 11:11:36 -0300

    alterando implementação para inserção no MongoDB

--- src/main/java/maven/arduino/waterFlowSensor/mongoDB/MongoDBConnection.java
+++ src/main/java/maven/arduino/waterFlowSensor/mongoDB/MongoDBConnection.java
@@ -1,12 +1,11 @@
 package maven.arduino.waterFlowSensor.mongoDB;
 
-import java.net.UnknownHostException;
+import org.bson.Document;
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 import maven.arduino.waterFlowSensor.domain.WaterFlowSensorDomain;
 
@@ -15,11 +14,11 @@ public class MongoDBConnection {
 	
 	private MongoClient mongoClient;
 	
-	//private MongoDatabase database;
+	private MongoDatabase database;
 
-	private DB database;
+	//private DB database;
 	
-	private DBCollection collection;
+	private MongoCollection<Document> collection;
 	
 	public MongoDBConnection() {
 	}
@@ -32,55 +31,56 @@ public class MongoDBConnection {
 		this.mongoClient = mongoClient;
 	}
 
-	/*public MongoDatabase getDatabase() {
+	public MongoDatabase getDatabase() {
 		return database;
 	}
 
 	public void setDatabase(MongoDatabase database) {
 		this.database = database;
-	}*/
-
-	public DB getDatabase() {
-		return database;
 	}
 
-	public void setDatabase(DB database) {
-		this.database = database;
-	}
+//	public DB getDatabase() {
+//		return database;
+//	}
+//
+//	public void setDatabase(DB database) {
+//		this.database = database;
+//	}
 
-	public DBCollection getCollection() {
+	public MongoCollection<Document> getCollection() {
 		return collection;
 	}
 
-	public void setCollection(DBCollection collection) {
+	public void setCollection(MongoCollection<Document> collection) {
 		this.collection = collection;
 	}	
 	
-	/*public void openConnection() {
-		MongoClientURI uri = new MongoClientURI("mongodb+srv://admin:admin@waterflowsensorcluster-qkxa6.gcp.mongodb.net/WaterFlowSensorDB?retryWrites=true&w=majority");
-		this.mongoClient = new MongoClient(uri);
-		this.database = mongoClient.getDatabase("WaterFlowSensorDB");
-	}*/
-
 	public void openConnection() {
-		try {
-			this.mongoClient = new MongoClient();
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		}
-		this.database = mongoClient.getDB("local");
-		this.collection = database.getCollection("startup_log");
+		MongoClientURI uri = new MongoClientURI("mongodb+srv://admin:admin@cluster0-qkxa6.mongodb.net/test?retryWrites=true&w=majority");
+		this.mongoClient = new MongoClient(uri);
+		this.database = mongoClient.getDatabase("waterFlowDB");
+		this.collection = database.getCollection("waterFlowCollection");
 	}
+
+//	public void openConnection() {
+//		try {
+//			this.mongoClient = new MongoClient();
+//		} catch (UnknownHostException e) {
+//			e.printStackTrace();
+//		}
+//		this.database = mongoClient.getDB("local");
+//		this.collection = database.getCollection("startup_log");
+//	}
 	
 	public void closeConnection() {
 		this.mongoClient.close();
 	}
 	
 	public void store(WaterFlowSensorDomain domain) {
-		DBObject dataCollected = new BasicDBObject("flowRate", domain.getValue())
+		Document dataCollected = new Document("flowRate", domain.getValue())
 				.append("userID", domain.getUser())
 				.append("deviceID", domain.getDeviceId())
 				.append("description", domain.getDescription());
-		this.collection.insert(dataCollected);
+		this.collection.insertOne(dataCollected);
 	}
 }
